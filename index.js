eec3c6a20766786ddb6f1e12afbc14e01a1394cb
patteg21 <patteg21@gmail.com>
2023-09-01 17:17:30 -0400

    up to date

--- index.js
+++ index.js
@@ -1,9 +1,51 @@
-const test = document.getElementById("test")
-const appendButton = document.getElementById("appendButton")
+// sets the variable for the movement piece
+const iMove = document.getElementById("i-move")
 
-appendButton.addEventListener("click",function(){
-    const childElement = document.createElement('h6')
-    childElement.textContent = "Worked"
-    
-    test.appendChild(childElement)
+// sets the position that iMove with be at
+let positionX = 0
+let positionY = 0
+
+function moveMe(){
+    // style for the header to transform 
+    iMove.style.transform = `translate(${positionX}px, ${positionY}px)`
+}
+
+// eventListern, listens for key inputs
+document.addEventListener('keydown', function(event){
+    // deals with which key was pressed and responds
+
+    // pixel change amount
+    const step = 10
+
+    switch (event.key) {
+        case 'ArrowLeft':
+            positionX -= step;
+            break;
+        case 'ArrowRight':
+            positionX += step;
+            break;
+        case 'ArrowUp':
+            positionY -= step;
+            break;
+        case 'ArrowDown':
+            positionY += step;
+            break;
+    }
+
+    moveMe()
+})
+
+// sets the current position of the header
+moveMe()
+
+
+// grads the area that contains blocks
+const blocks = document.getElementById('blocks')
+
+// listens for when the blocks are clicked
+blocks.addEventListener('click',function(){
+    const block = document.createElement('div')
+    block.id = "block"
+    block.className = "block"
+    blocks.appendChild(block)
 })
